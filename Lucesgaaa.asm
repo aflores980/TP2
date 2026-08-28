1fc9615a45e0241d64152f4ed00823074a833c71
aflores980 <aflores980@mi.unc.edu.ar>
2026-08-28 20:15:58 -0300

    Codigo

--- Lucesgaaa.asm
+++ Lucesgaaa.asm
@@ -9,17 +9,17 @@
 ;===============================================================================
 
 ;===============================================================================
-; DIRECTIVAS DE INCLUSI?N
+; DIRECTIVAS DE INCLUSION
 ;===============================================================================
 LIST P=16F887			
 #include "p16f887.inc"	
 	
 ;===============================================================================
-; CONFIGURACI?N GENERAL DEL MCU
+; CONFIGURACION GENERAL DEL MCU
 ;=============================================================================== 	
 __CONFIG _XT_OSC, _WDTE_OFF, _MCLRE_ON, _LVP_OFF
 ;===============================================================================
-; DEFINICI?N DE CONSTANTES
+; DEFINICION DE CONSTANTES
 ;===============================================================================     
 #DEFINE BUZZER	PORTC, 0
     
@@ -38,7 +38,7 @@ DELAY1_Init	EQU .64
 DELAY2_Init	EQU .51
 DELAY3_Init	EQU .10
 ;===============================================================================
-; DEFINICI?N DE VARIABLES
+; DEFINICION DE VARIABLES
 ;=============================================================================== 
     CBLOCK 0X20
     DELAY1
@@ -48,9 +48,9 @@ DELAY3_Init	EQU .10
     COUNTER_SECUENCES
     ENDC
 ;===============================================================================
-; DECLARACI?N DE MACROS PARA CONFIGURACI?N DE REGISTROS
+; DECLARACION DE MACROS PARA CONFIGURACION DE REGISTROS
 ;===============================================================================
-CFG_SWITCH MACRO
+CFG_SWITCH MACRO      ;Seteo de el puerto 5 ansel a salida digital, esto para el pulsador.
     BANKSEL ANSEL
     BCF	     ANSEL, 5
     
@@ -60,46 +60,57 @@ CFG_SWITCH MACRO
     BANKSEL PORTE
     BCF     SWITCH
 ENDM
-CFG_DIGITS_DSPL MACRO
+
+;Este trabajo práctico carece de Display, por lo que no se hace nada en nunguna de estas macros.
+CFG_DIGITS_DSPL MACRO 
 ENDM
 DSPL_ALL_OFF MACRO
 ENDM
-CFG_LEDS MACRO
+
+CFG_LEDS MACRO       ;Seteo para que los leds empiecen desactivados.
     BANKSEL TRISD
     CLRF    TRISD
     
     BANKSEL PORTD
     CLRF    PORTD
 ENDM
-LEDS_ON MACRO
+
+LEDS_ON MACRO        ;Seteo para que todos los leds se enciendan.
     BANKSEL PORTD
     MOVLW b'11111111'
     MOVWF PORTD
 ENDM
-LEDS_OFF MACRO
+
+LEDS_OFF MACRO       ;Seteo para que todos los leds se apaguen.
     BANKSEL PORTD
     CLRF    PORTD
 ENDM
+
 LEDS_RLF MACRO
 ENDM
+
 LEDS_RRF MACRO
 ENDM
-CFG_BUZZER MACRO
+
+CFG_BUZZER MACRO     ;Asignacion de buzzer como salida en el puerto C en estado apagado.
     BANKSEL TRISC
     BCF     BUZZER
     
-    BANKSEL PORTD
+    BANKSEL PORTC
     BCF     BUZZER
 ENDM
-BUZZER_ON MACRO
+
+BUZZER_ON MACRO      ;Enciende el buzzer.
     BANKSEL PORTC
     BSF     BUZZER
 ENDM
-BUZZER_OFF MACRO
+
+BUZZER_OFF MACRO     ;Apaga el buzzer
     BANKSEL PORTC
     BCF     BUZZER
 ENDM
-CFG_DELAY_100MS MACRO
+
+CFG_DELAY_100MS MACRO    ;Genera un delay de 100ms.
     LOCAL OUTER_LOOP, MIDDLE_LOOP, INSIDE_LOOP
     MOVLW   DELAY3_Init
     MOVWF   DELAY3
@@ -117,32 +128,34 @@ CFG_DELAY_100MS MACRO
 	DECFSZ	DELAY3
 	GOTO	OUTER_LOOP
 ENDM
-CFG_DELAY_200MS MACRO
+
+CFG_DELAY_200MS MACRO ;Simplemente llama 2 veces la macro CFG_DELAY_100MS para generar un delay de 200ms.
     CFG_DELAY_100MS
     CFG_DELAY_100MS
 ENDM
-CFG_DELAY_300MS MACRO
+
+CFG_DELAY_300MS MACRO ;Llama 2 macros para generar un delay de 300ms.
     CFG_DELAY_200MS
     CFG_DELAY_100MS
 ENDM
-CFG_DELAY_1s MACRO
+CFG_DELAY_1s MACRO   ;Finalmente, llama utilizando las otras macros para generar un delay de 1s.
     CFG_DELAY_300MS
     CFG_DELAY_300MS
     CFG_DELAY_300MS
     CFG_DELAY_100MS
 ENDM
-CFG_SECUENCES MACRO	;4 SECUENCIAS
+CFG_SECUENCES MACRO	 ;4 secuencias.
     
 ENDM
 ;===============================================================================
-; INICIALIZACI?N DEL MCU (C?DIGO ABSOLUTO)
+; INICIALIZACI?N DEL MCU (CODIGO ABSOLUTO)
 ;===============================================================================    
     ORG     0x00	;Vector de Reset
     GOTO    INICIO	;Salto al inicio del programa principal
     ORG     0x05	;Ubicaci?n Programa Principal en la memoria 
 			;de programa
 ;===============================================================================
-; INICIALIZACI?N DE MACROS PARA CONFIGURACI?N DE REGISTROS
+; INICIALIZACION DE MACROS PARA CONFIGURACI?N DE REGISTROS
 ;===============================================================================    	    
 
 ;===============================================================================
@@ -152,12 +165,72 @@ INICIO
    
 
 MAIN_LOOP
-   
-   GOTO MAIN_LOOP
+	TEST_LEDS      ;Estado neutro de los leds mientras no se precione el pulsador (se encienden y apagan con intervalos de 1s)
+		CFG_DELAY_1s
+		LEDS_ON
+		DELAY_3LOOP
+		LEDS_OFF
+		DELAY3_LOOP
+	BTFSC BUZZER
+	GOTO SECUENCIA
+	GOTO MAIN_LOOP
 	
 ;===============================================================================
 ; SUBRUTINAS
-;===============================================================================	 
+;===============================================================================
+
+BUZZER_BIP	;completar mapa primero
+
+FORWARD_LED					;Leds se prenden uno a uno de izq a der
+STC							;Flag de carry en 1
+FW_LOOP
+	LEDS_RLF				;Movimiento de leds izq a der
+	DELAY_3LOOP
+	BTFSS LED7				;Revisa si el led 7 está en 1 para ver si terminó la secuencia
+RETURN
+
+BACKWARD_LED				;Leds se prenden uno a uno de der a izq
+CLC							;Flag de carry en 0
+BW_LOOP
+	LEDS_RRF				;Movimiento de leds izq a der
+	DELAY_3LOOP
+	BTFSS LED0				;Revisa si el led 0 está en 1 para ver si terminó la secuencia
+RETURN
+
+PROGRESSIVE_LED_ON	;completar mapa primero
+
+PROGRESSIVE_LED_OFF ;completar mapa primero
+
+RUNNING_LIGHT
+CFG_DELAY_300ms					;Configura tiempo de delay de DELAY_3LOOP
+LEDS_OFF
+LOOP_RL
+	FORWARD_LED
+	DCFSZ COUNTER_SEQUENCES		;cantidad de repeticiones de la accion anterior
+	GOTO LOOP_RL
+CFG_SEQUENCES					;reconfigura variables que necesitan las secuencias
+RETURN
+
+BIDIR_RUNNING_LIGHT
+CFG_DELAY_200ms
+LEDS_OFF
+LOOP_BRL
+	FORWARD_LED
+	BACKWARD_LED
+	DECFSZ COUNTER_SEQUENCES	;cantidad de repeticiones de la accion anterior
+CFG_SEQUENCES					;reconfigura variables que necesitan las secuencias
+RETURN
+
+CRAWLING
+CFG_DELAY_100ms
+LEDS_OFF
+LOOP_CW
+	PROGRESSIVE_LED_ON
+	PROGRESSIVE_LED_OFF
+	DECFSZ COUNTER_SEQUENCES	;cantidad de repeticiones de la accion anterior
+CFG_SEQUENCES					;reconfigura variables que necesitan las secuencias
+RETURN
+
 ;***************************
 ; @brief    LOOP DE UNA CANTIDAD DE CICLOS PARA GENERAR UN DELAY
 ;           
@@ -166,6 +239,7 @@ MAIN_LOOP
 ;	    LA CANTIDAD DE CICLOS. UNA VEZ TERMINADO EL LOOP, VUELVE A AL
 ;	    MAIN LOOP. USO 2 NOP PARA AJUSTAR Y QUE QUEDE EXACTAMENTE 1 ms
 ;*************************** 
+SECUENCIA
 
 RETURN			; (2 ciclos)
 ;===============================================================================		
